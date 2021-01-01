76a1e1e63ef250b4a7953f003c6371fc9825e022
brianrt <bthomp2000@gmail.com>
2021-01-01 02:37:09 -0600

    Add playback speed control, make vowels slightly longer

--- Text to Buzz/UserInterface/MainViewController.swift
+++ Text to Buzz/UserInterface/MainViewController.swift
@@ -23,6 +23,11 @@ class MainViewController: UIViewController {
     @IBOutlet weak var motor3View: UIView!
     @IBOutlet weak var motor4View: UIView!
     
+    // Playback speed UI
+    @IBOutlet weak var playbackStepper: UIStepper!
+    @IBOutlet weak var playbackSpeedLabel: UILabel!
+    
+    
     // Motor value array
     var motorValueViews: [UIView]!
     var initialMotorViewHeight: CGFloat!
@@ -30,6 +35,9 @@ class MainViewController: UIViewController {
     
     // Current motor state
     var motorValues: [UInt8] = [0,0,0,0]
+    
+    // Time allowed for each phoneme vibration
+    var playBackSpeed = 1.0
     
     // Class instances
     var buzz: Buzz!
@@ -52,7 +60,7 @@ class MainViewController: UIViewController {
         self.trainingView.isHidden = true
         self.textInput.delegate = self
         
-        //Setup motorViews
+        // Setup motorViews
         self.motorValueViews = [motor1View, motor2View, motor3View, motor4View]
         initialMotorViewHeight = motor1View.frame.height
         initialMotorViewY = motor1View.frame.origin.y
@@ -61,11 +69,17 @@ class MainViewController: UIViewController {
         motor2View.layer.cornerRadius = 4
         motor3View.layer.cornerRadius = 4
         motor4View.layer.cornerRadius = 4
+        
+        // Setup stepper
+        playbackStepper.wraps = true
+        playbackStepper.autorepeat = true
+        playbackStepper.minimumValue = 1
+        playbackStepper.maximumValue = 5
+        playbackStepper.value = 1
     }
     
     private func initializeMotors() {
         motorValues = [0,0,0,0]
-        
     }
 
     @IBAction func didPressConnectToBuzz(_ sender: Any) {
@@ -85,6 +99,7 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func didPressPlaySentence(_ sender: Any) {
+        playbackStepper.isEnabled = false
         var seconds = 0.0
         self.phonemeLabel.text = ""
         let sentencePhonemes = self.phonemes.sentenceToPhonemes(sentence: textInput.text!.trimmingCharacters(in: .whitespacesAndNewlines))
@@ -93,12 +108,17 @@ class MainViewController: UIViewController {
             let phonemes = wordPhonemes.1
             for phoneme in phonemes {
                 Timer.scheduledTimer(timeInterval: seconds, target: self, selector: #selector(playPhoneme(sender:)), userInfo: phoneme, repeats: false)
-                seconds += 0.4
+                var phonemeTime = 0.8 / playBackSpeed
+                // Make vowels longer
+                if self.phonemes.isVowel(phoneme: phoneme) {
+                    phonemeTime *= 1.3
+                }
+                seconds += phonemeTime
                 Timer.scheduledTimer(timeInterval: seconds, target: self, selector: #selector(zeroMotors), userInfo: phoneme, repeats: false)
-                seconds += 0.125
+                seconds += 0.2 / playBackSpeed
             }
         }
-        Timer.scheduledTimer(timeInterval: seconds+0.5, target: self, selector: #selector(endSentence), userInfo: nil, repeats: false)
+        Timer.scheduledTimer(timeInterval: seconds, target: self, selector: #selector(endSentence), userInfo: nil, repeats: false)
     }
     
     @objc func playPhoneme(sender: Timer) {
@@ -116,6 +136,7 @@ class MainViewController: UIViewController {
     @objc func endSentence() {
         self.phonemeLabel.text = ""
         self.buzz.vibrateMotors(motorValues: [0,0,0,0])
+        self.playbackStepper.isEnabled = true
     }
     
     func updateMotorHeight(motorValues: [UInt8]) {
@@ -127,12 +148,12 @@ class MainViewController: UIViewController {
             let newHeight = heightRatio * initialMotorViewHeight
             let newY = initialMotorViewY + (yRatio * initialMotorViewHeight)
             let motorView = motorValueViews[i]
-            UIView.animate(withDuration: 0.25) {
+            UIView.animate(withDuration: min(0.5 / playBackSpeed, 0.25)) {
                 motorView.frame = CGRect(x: motorView.frame.origin.x, y: newY, width: motorView.frame.width, height: newHeight )
             }
         }
     }
-    
+        
     @IBAction func didPressReleaseBuzz(_ sender: Any) {
         self.trainingView.isHidden = true
         self.buzz.vibrateMotors(motorValues: [0,0,0,0])
@@ -142,6 +163,12 @@ class MainViewController: UIViewController {
     @IBAction func didPressClear(_ sender: Any) {
         self.textInput.text = ""
     }
+    
+    @IBAction func didPressStepper(_ sender: Any) {
+        self.playBackSpeed = playbackStepper.value
+        self.playbackSpeedLabel.text = String(Int(playbackStepper.value))
+    }
+    
     
 }
 
